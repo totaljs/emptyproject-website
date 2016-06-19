c8724aba4c6188725ad6033b30bcaf2b0e3a3906
Peter Širka <petersirka@gmail.com>
2016-06-19 09:19:44 +0200

    New changes.

--- public/js/default.js
+++ public/js/default.js
@@ -40,3 +40,23 @@ COMPONENT('newsletter', function() {
 		});
 	};
 });
+
+COMPONENT('cookie', function() {
+	var self = this;
+	self.readonly();
+	self.singleton();
+	self.make = function() {
+		var cookie = localStorage.getItem('cookie');
+		if (cookie) {
+			self.element.addClass('hidden');
+			return;
+		}
+
+		self.element.removeClass('hidden').addClass('ui-cookie');
+		self.element.append('<button>' + (self.attr('data-button') || 'OK') + '</button>');
+		self.element.on('click', 'button', function() {
+			localStorage.setItem('cookie', '1');
+			self.element.addClass('hidden');
+		});
+	};
+});
